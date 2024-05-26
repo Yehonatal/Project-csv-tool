8b695ae6b51c73359577e0bf3420e6aae0f79e59
Yonatan Afewerk <yonatanafewerk@gmail.com>
2024-05-26 15:42:53 +0300

    Dev: menu logic update

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 display_menu() {
-    whiptail --title "CSV Data Analysis Tool" --menu "Select an option:" 15 60 3 \
+    whiptail --title "CSV Data Analysis Tool" --menu "Select an option:" 15 60 2 \
     "1" "Input CSV file" \
     "2" "Analyze CSV file" \
-    "3" "Exit" \
     3>&1 1>&2 2>&3
 }
 
@@ -14,7 +13,6 @@ while true; do
     case $choice in
         1) ./input_csv.sh ;;
         2) ./analysis_functions.sh ;;
-        3) whiptail --title "Exiting" --msgbox "Exiting CSV Data Analysis Tool." 8 40; exit ;;
-        *) whiptail --title "Invalid Choice" --msgbox "Please select a valid option." 8 40 ;;
+        *) exit ;;
     esac
 done
